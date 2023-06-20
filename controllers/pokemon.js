14b30ff669c26407bc89aefc28a26a27790532dd
nicholasjamessmith <njsmith6@gmail.com>
2023-06-20 14:55:37 -0400

    basic crud functionality

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -15,6 +15,7 @@ router.get("/", (req, res) => {
 router.get("/new", (req, res) => {
   res.render("pokemon/new.ejs")
 })
+
 //Create Route POST /soda -> creates a new soda, redirect back to index
 router.post("/", (req, res) => {
   Pokemon.create(req.body)
@@ -23,8 +24,25 @@ router.post("/", (req, res) => {
 })
 
 //Edit Route POST /pokemon/:id/edit -> create form to update pokemon
+router.get("/:id/edit", (req, res) => {
+  res.render("pokemon/edit.ejs", {
+    pokemon: Pokemon.getOne(req.params.id),
+    index: req.params.id
+  })
+})
 
 //Update Route PUT /soda/:id -> receives form data, update soda, redirectss to index
+router.put("/:id", (req, res) => {
+  Pokemon.update(req.params.id, req.body)
+  res.redirect("/pokemon")
+  //res.json(req.body)
+})
+
+//Destroy Route Delete
+router.delete("/:id", (req, res) => {
+  Pokemon.destroy(req.params.id)
+  res.redirect("/pokemon")
+})
 
 //Show Route /pokemon/:id -> page on individual pokemon
 router.get("/:id", (req, res) => {
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config() //reaad the .env file process.env (process.eng.PORT)
 const express = require("express")
 const morgan = require("morgan") // logger
 const PokemonRouter = require("./controllers/pokemon") //import the router
+const methodOverride = require("method-override")
 
 //Create Application Object
 const app = express() // create our application object
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 3777
 
 //Middleware
 app.use(morgan("tiny"))
+app.use(methodOverride("_method"))
 app.use("/static", express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 app.use("/pokemon", PokemonRouter)
